3acd553e4f872d7d00469e50b044bc3b106a3777
dodyvirgiawan <dody.virgiawan97@gmail.com>
2021-09-28 20:41:13 +0700

    add testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,140 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Dashboard page', () => {
+    test('Should render sidebar correctly', () => {
+        render(<App />)
+
+        screen.getByText('Dashboard')
+        screen.getByText('Saved Candidates')
+        screen.getByText('Todos')
+    })
+
+    test('Should render correct section in dashboard page', () => {
+        render(<App />)
+
+        screen.getByText('Statistics')
+        screen.getByText('Candidate List')
+    })
+
+    test('Should return candidate list data from API', async () => {
+        render(<App />)
+
+        await screen.findByText('No.', undefined, { timeout: 5000 })
+
+        screen.getAllByText('Details')
+        screen.getAllByText('Accept')
+        screen.getAllByText('Decline')
+    })
+
+    test('Should go to saved candidate page when user click the saved candidates button in dashboard page', async () => {
+        render(<App />)
+
+        const savedCandidateButton = screen.getByText('Saved Candidates')
+        fireEvent.click(savedCandidateButton)
+
+        const sampleSavedCandidate = screen.queryByTestId('savedCandidate')
+        const emptySavedCandidate = screen.queryByTestId('emptySavedCandidate')
+
+        expect(sampleSavedCandidate).not.toBeInTheDocument()
+        expect(emptySavedCandidate).toBeInTheDocument()
+    })
+
+    test('Should go to detail page when user click the detail button in dashboard page', async () => {
+        render(<App />)
+
+        const dashboardButton = screen.getByText('Dashboard')
+        fireEvent.click(dashboardButton)
+
+        await screen.findByText('No.', undefined, { timeout: 5000 })
+
+        const candidateDetailButton = await screen.queryAllByTestId('detailButton', undefined, {
+            timeout: 5000,
+        })
+
+        fireEvent.click(candidateDetailButton[0])
+
+        await screen.findByText('Candidate ID', { timeout: 5000 })
+
+        screen.getByText('Candidate Username')
+        screen.getByText('Candidate Email')
+        screen.getByText('Candidate Phone')
+        screen.getByText('Candidate Website')
+        screen.getByText('Candidate Past Company')
+        screen.getByText('Candidate Address')
+        screen.getByText('Candidate Locations')
+
+        screen.getByRole('button', { name: 'Accept Candidate' })
+        screen.getByRole('button', { name: 'Decline Candidate' })
+        const saveButton = screen.getByRole('button', { name: 'Save this candidate' })
+
+        fireEvent.click(saveButton)
+    })
+})
+
+describe('Favorite page', () => {
+    test('Should be able to show saved candidates properly', () => {
+        render(<App />)
+
+        const savedCandidateButton = screen.getByText('Saved Candidates')
+        fireEvent.click(savedCandidateButton)
+
+        const sampleSavedCandidate = screen.queryByTestId('savedCandidate')
+        const emptySavedCandidate = screen.queryByTestId('emptySavedCandidate')
+
+        expect(sampleSavedCandidate).toBeInTheDocument()
+        expect(emptySavedCandidate).not.toBeInTheDocument()
+    })
+
+    test('Should be able to click the favorite candidate card, and render detail page', async () => {
+        render(<App />)
+
+        const candidateDetailButton = await screen.queryAllByTestId('savedCandidate', undefined, {
+            timeout: 5000,
+        })
+
+        fireEvent.click(candidateDetailButton[0])
+
+        await screen.findByText('Candidate ID', { timeout: 5000 })
+
+        screen.getByText('Candidate Username')
+        screen.getByText('Candidate Email')
+        screen.getByText('Candidate Phone')
+        screen.getByText('Candidate Website')
+        screen.getByText('Candidate Past Company')
+        screen.getByText('Candidate Address')
+        screen.getByText('Candidate Locations')
+
+        screen.getByRole('button', { name: 'Accept Candidate' })
+        screen.getByRole('button', { name: 'Decline Candidate' })
+        screen.getByRole('button', { name: 'Save this candidate' })
+    })
+})
+
+describe('Todo page', () => {
+    test('Should render correct section in todo page', () => {
+        render(<App />)
+
+        const todoButton = screen.getByText('Todos')
+        fireEvent.click(todoButton)
+
+        screen.getByText('Statistics')
+        screen.getByText('Todo List')
+    })
+
+    test('Should render todo list data from API', async () => {
+        render(<App />)
+
+        await screen.findByText('No.', undefined, { timeout: 5000 })
+
+        screen.getAllByText('Incomplete')
+        screen.getAllByText('Completed')
+    })
+
+    test('Should render pagination buttons', () => {
+        render(<App />)
+
+        screen.getByText('Previous')
+        screen.getByText('Next')
+    })
+})
--- src/components/CandidateCard.jsx
+++ src/components/CandidateCard.jsx
@@ -7,7 +7,10 @@ import { BsFillPersonFill } from 'react-icons/bs'
 export default function CandidateCard({ candidate }) {
     return (
         <Link to={`/candidates/${candidate.id}`}>
-            <div className="container bg-gray-200 w-72 p-3 rounded-xl md:flex md:flex-row md:ml-3 mx-auto mt-5 h-full">
+            <div
+                className="container bg-gray-200 w-72 p-3 rounded-xl md:flex md:flex-row md:ml-3 mx-auto mt-5 h-full"
+                data-testid="savedCandidate"
+            >
                 <div className="container md:w-1/3 m-auto">
                     <BsFillPersonFill className="mt-1 w-10 h-10 mx-auto" />
                 </div>
